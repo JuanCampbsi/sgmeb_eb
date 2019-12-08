bfc0109db210b4faabbe4bed3cfd8ddbe6de26be
JuanCampbsi <juancampos.bsi@outlook.com>
2019-12-07 22:11:16 -0300

    Criando pacote anotação e classe genérica para buscar

--- src/br/com/project/annotation/IdentificaCampoPesquisa.java
+++ src/br/com/project/annotation/IdentificaCampoPesquisa.java
@@ -1,8 +1,10 @@
 package br.com.project.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-
-import org.hibernate.annotations.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Annotation para identificar os campos que podem aparecer na pesquisa das
@@ -11,11 +13,13 @@ import org.hibernate.annotations.Target;
  * @author Juan Campos
  *
  */
-@Target(value = { java.lang.annotation.ElementType.FIELD })
-@Retention(value = java.lang.annotation.RetentionPolicy.RUNTIME)
-@java.lang.annotation.Documented
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
+@Documented
 public abstract @interface IdentificaCampoPesquisa {
-	String descricaoCampo(); // descri��o do campos para a tela
+	String descricaoCampo(); // descricao do campos para a tela
 	String campoConsulta(); // campo do banco
-	int principal() default 10000; // posi��o que vai aparecer no combo, 10000 � para que o default seja colocado depois dos que s�o estabelecidos
+	int principal() default 10000; // posicao que vai aparecer no combo, 10000
+									// para que o default seja colocado depois
+									// dos que sao estabelecidos
 }
