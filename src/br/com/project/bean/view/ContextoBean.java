d4d38c83b3aaf6e867239cefcc2f729d3ae2d5c4
Juan <juan12_jovem@outlook.com>
2019-12-15 21:24:59 -0300

    Adicionando classes de repository, incluindo codigo contexto bean, alterando template principal do sistema

---
+++ src/br/com/project/bean/view/ContextoBean.java
@@ -0,0 +1,108 @@
+package br.com.project.bean.view;
+
+import java.io.Serializable;
+
+import javax.annotation.Resource;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+
+
+import br.com.project.geral.controller.EntidadeController;
+//import br.com.project.geral.controller.EntidadeController;
+import br.com.project.geral.controller.SessionController;
+import br.com.project.model.classes.Entidade;
+
+@Scope(value = "session")
+@Component(value = "contextoBean")
+public class ContextoBean implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private static final String USER_LOGADO_SESSAO = "userLogadoSessao";
+	
+	@Autowired
+	private EntidadeController entidadeController;
+	
+	@Resource
+	private SessionController sessionController;
+
+	
+
+	/**
+	 * 
+	 * @return String Usuário logado remote user
+	 */
+	public String getUserLogado() {
+		return getExternalContext().getRemoteUser();
+	}
+
+	/**
+	 * 
+	 * @return String Usuário logado user principal
+	 */
+	public String getUserPrincipal() {
+		return getExternalContext().getUserPrincipal().getName();
+	}
+
+	/**
+	 * 
+	 * @return String Usuário logado Authentication Spring security
+	 */
+	public Authentication getAuthentication() {
+		return SecurityContextHolder.getContext().getAuthentication();
+	}
+
+	/**
+	 * 
+	 * @return ExternalContext da application
+	 */
+	public ExternalContext getExternalContext() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext external = context.getExternalContext();
+		return external;
+	}
+
+	/**
+	 * @return Entidade logado na sessão atualmente
+	 */
+	public Entidade getEntidadeLogada() throws Exception {
+		Entidade entidade = (Entidade) getExternalContext().getSessionMap().get(USER_LOGADO_SESSAO);
+		
+		if (entidade == null || (entidade != null && !entidade.getEnt_login().equals(getUserPrincipal()))) {
+			if (getAuthentication().isAuthenticated()) {
+				entidadeController.updateUltimoAcessoUser(getAuthentication().getName());
+				entidade = entidadeController.findUserLogado(getAuthentication().getName());
+				getExternalContext().getSessionMap().put(USER_LOGADO_SESSAO, entidade);
+				sessionController.addSession(entidade.getEnt_login(), (HttpSession) getExternalContext().getSession(false));
+			}
+		}
+		return entidade;
+	}
+	
+	/**
+	 * Retorna se o usuario logado possui os acesso passados como paramentro, este metodo destina-se ao uso onde não é possivel usar
+	 * as tags do spring security
+	 */
+	public boolean possuiAcesso(String... acessos) {
+		for (String acesso : acessos) {
+			for (GrantedAuthority autorizacao : getAuthentication().getAuthorities()){ 
+				if (autorizacao.getAuthority().trim().equals(acesso.trim())){
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	
+
+}
---
+++ src/br/com/project/bean/view/EntidadeBeanView.java
@@ -0,0 +1,33 @@
+package br.com.project.bean.view;
+
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import br.com.project.util.all.BeanViewAbstract;
+
+@Controller
+@Scope(value = "session")
+@ManagedBean(name = "entidadeBeanView")
+public class EntidadeBeanView  extends BeanViewAbstract{
+	
+	@Autowired
+	private ContextoBean contextoBean;
+	
+	private static final long serialVersionUID = 1L;
+	
+	public String getUsuarioLogadoSecurity(){
+		return contextoBean.getAuthentication().getName();
+	}
+	
+	public Date getUltimoAcesso() throws Exception {
+		return contextoBean.getEntidadeLogada().getEnt_ultimoacesso();
+	}
+	
+
+}
+
